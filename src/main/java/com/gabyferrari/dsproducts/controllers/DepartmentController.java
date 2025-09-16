3f68a57501b94e54b9bc36389c03f08fb70e6611
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-16 12:29:22 -0300

    GET department by id

--- src/main/java/com/gabyferrari/dsproducts/controllers/DepartmentController.java
+++ src/main/java/com/gabyferrari/dsproducts/controllers/DepartmentController.java
@@ -1,10 +1,12 @@
 package com.gabyferrari.dsproducts.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,12 @@ public class DepartmentController {
 	public ResponseEntity<List<DepartmentDTO>> findAll() {
 		List<DepartmentDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id) {
+		DepartmentDTO obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
--- src/main/java/com/gabyferrari/dsproducts/services/DepartmentService.java
+++ src/main/java/com/gabyferrari/dsproducts/services/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.gabyferrari.dsproducts.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.gabyferrari.dsproducts.model.dto.DepartmentDTO;
 import com.gabyferrari.dsproducts.model.entities.Department;
 import com.gabyferrari.dsproducts.repositories.DepartmentRepository;
+import com.gabyferrari.dsproducts.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
@@ -19,5 +22,11 @@ public class DepartmentService {
 	public List<DepartmentDTO> findAll() {
 		List<Department> list = repository.findAll();
 		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+	
+	public DepartmentDTO findById(UUID id) {
+		Optional<Department> result = repository.findById(id);
+		Department entity = result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+		return new DepartmentDTO(entity);
 	}
 }
