0f3d2ff3162e2d419960743fb3a268195d861671
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-16 00:43:09 -0300

    Initial setup

---
+++ src/main/java/com/gabyferrari/dsproducts/DsproductsCassandraNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.dsproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsproductsCassandraNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsproductsCassandraNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabyferrari/dsproducts/DsproductsCassandraNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.dsproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsproductsCassandraNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
